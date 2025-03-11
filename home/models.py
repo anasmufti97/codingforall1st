74580c1fc5cd088a58a870b502391248a3f8cddb
ANAS MUFTI <muftianas97@gmail.com>
2025-03-11 14:26:23 +0500

    ...

--- home/models.py
+++ home/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Person(models.Model):
+
+    
     name = models.CharField(max_length=100)
+    
     age = models.IntegerField()
+    
     email = models.EmailField(unique=True)
+    
     phone_number = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
+    
         return self.name
