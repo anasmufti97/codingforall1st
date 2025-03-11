aec8953f2c0ca79e57e3bb5914445f60e517fa34
ANAS MUFTI <muftianas97@gmail.com>
2025-03-11 14:26:02 +0500

    ...

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
